76d5b9c6363ce8b7f93edc384df20f79a77ab851
brh55 <brandonhim@live.com>
2017-07-10 09:33:04 -0700

    Add securityContext and validate with lowercase

--- components/header/securityContext.js
+++ components/header/securityContext.js
@@ -1,20 +1,36 @@
 'use strict';
+// Security context provides some pre-validation
+// to prevent unnecessary request sent to the wire
+// create note about passwords in clear text should be done through HTTPS
+// LINK: https://developer.cisco.com/site/webex-developer/develop-test/xml-api/api-guide/#global-request-elements-in-security-context
 
-const xmlBuilder = require('./libs/xmlBuilder')('securityContext');
+const xmlBuilder = require('../../libs/xmlBuilder')('securityContext');
+const mapKeys = require('lodash.mapkeys');
+
 /**
  * Generates the security context XML
- * @param  {object} attributes
- * @param  {string} attributes.sitename
- * @param  {string} attributes.webExID
- * @param  {string} attributes.password
- * @param  {string} attributes.partnerID
- * @param  {string} attributes.email
+ * @param  {object} elements
+ * @param  {string} elements.siteName || elements.siteID
+ * @param  {string} elements.webExID ||  elements.email
+ * @param  {string} elements.password || elements.sessionTicket
+ * @param  {string} elements.partnerID
  * @return {string}            xml <securityContext> string
  */
-module.exports = (attributes) => {
-	if (!attributes.siteName || !attributes.webExID || !attributes.password) {
-		throw new Error('Missing required attributes');
+module.exports = (elements) => {
+	const eCopy = mapKeys(elements, (v, k) => k.toLowerCase());
+
+	// Throw error if one of the two don't exists
+	if (!eCopy.sitename && !eCopy.siteid) {
+		throw new Error('Missing required keys: siteName or siteID');
 	}
-	
-	return xmlBuilder.buildObject(attributes);
+
+	if (!eCopy.webexid && !eCopy.email) {
+		throw new Error('Missing required keys: email or webExID');
+	}
+
+	if (!eCopy.password && !eCopy.sessionTicket) {
+		throw new Error('Missing required keys: password or sessionTicket');
+	}
+
+	return xmlBuilder.buildObject(elements);
 };
--- libs/validate.js
+++ libs/validate.js
@@ -1,10 +1,15 @@
-const has = require('lodash.has');
-
+// @TODO allow case insensitive
+// This function will return a list of required keys that are missing
 module.exports = (srcObj, keys) => {
     // Show the ones that aren't available in the object
-    const missingReqKeys = keys.filter((acc, v, k) => !has(srcObj, k));
+    // handles lowercase
+    const srcObjKeys = Object.keys(srcObj).map(item => item.toLowerCase());
+    const has = key => srcObjKeys.indexOf(key) !== -1;
+    const missingReqKeys = keys.filter((acc, v, k) => !has(k));
+
     if (missingReqKeys.length > 0) {
       throw new Error(`Missing required keys: ${missingReqKeys.toString().replace(',', ' ,')}`);
     }
+
     return null;
 }
