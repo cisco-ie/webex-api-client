0d17fb57d653efcbdc67e0f16bbb7e90fc9eb812
brh55 <brandonhim@live.com>
2017-07-11 11:25:56 -0700

    Fix name of test

--- tests/libs/valid-type.test.js
+++ tests/libs/valid-type.test.js
@@ -1,8 +1,8 @@
 import test from 'ava';
 import validType from '../../libs/valid-type';
 
-test('WebEx Time', t => {
-	const valid = [1, 2, 3];
+test('Valid Type', t => {
+	const validTypes = [1, 2, 3];
 	const error = t.throws(() => validType(valid, 4));
 	t.is(error.message, 'Expected a valid type (1, 2, 3), received 4');
 });
