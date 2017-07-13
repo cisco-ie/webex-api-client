7c0425f6dcc25c6f843283e0ae4c2f1c08e45c55
brh55 <brandonhim@live.com>
2017-07-13 10:03:03 -0700

    Update encoding

--- tests/client.test.js
+++ tests/client.test.js
@@ -7,12 +7,12 @@ import mock from './mocks/create-meeting.js'
 const TESTURL = 'https://test.com';
 
 nock('https://test.com')
-	.post('/webex', '<?xml version="1.0" encoding="UTF-8" standalone="yes"?><serv:message><header><securityContext><webExID>testuser</webExID><password>password123</password><siteId>tester</siteId></securityContext></header><body><bodyContent><body><metaData><confName>Sample Meeting</confName><meetingType>1</meetingType><agenda>Test</agenda></metaData><participants><name>James Kirk</name><email>JKirk@sz.webex.com</email></participants><schedule><startDate>05/31/2004 10:10:10</startDate><openTime>900</openTime><joinTeleconfBeforeHost>true</joinTeleconfBeforeHost><duration>20</duration><timezoneID>4</timezoneID></schedule></body></bodyContent></body></serv:message>')
+	.post('/webex', '<?xml version="1.0" encoding="ISO-8859-1"?><serv:message><header><securityContext><webExID>testuser</webExID><password>password123</password><siteId>tester</siteId></securityContext></header><body><bodyContent><body><metaData><confName>Sample Meeting</confName><meetingType>1</meetingType><agenda>Test</agenda></metaData><participants><name>James Kirk</name><email>JKirk@sz.webex.com</email></participants><schedule><startDate>05/31/2004 10:10:10</startDate><openTime>900</openTime><joinTeleconfBeforeHost>true</joinTeleconfBeforeHost><duration>20</duration><timezoneID>4</timezoneID></schedule></body></bodyContent></body></serv:message>')
 	.reply(200, '<Success />');
 
 // nock('http://localhost/path').post('').reply(200, 'hi');
 
-test('Create Meeting', t => {
+test('Create Meeting', async t => {
 	t.plan(1);
 
 	const webex = new Client({
@@ -21,30 +21,35 @@ test('Create Meeting', t => {
 		siteId: 'tester'
 	}, TESTURL + '/webex');
 
-	webex
-		.metaData({
-			confName: 'Sample Meeting',
-			meetingType: 1,
-			agenda: 'Test'
-		})
-		.participants([
-			{
-					name: 'James Kirk',
-					email: 'JKirk@sz.webex.com'
-				}
-		])
-		.schedule({
-			startDate: new Date(2004, 4, 31, 10, 10, 10),
-			openTime: 900,
-			joinTeleconfBeforeHost: true,
-			duration: 20,
-			timezoneID: 4
-		})
-		.createMeeting()
-		.then(resp => {
-			console.log(resp);
-			t.is(resp, '<Success />')
-		});
+	try {
+		const resp = await webex
+			.metaData({
+				confName: 'Sample Meeting',
+				meetingType: 1,
+				agenda: 'Test'
+			})
+			.participants([
+				{
+						name: 'James Kirk',
+						email: 'JKirk@sz.webex.com'
+					}
+			])
+			.schedule({
+				startDate: new Date(2004, 4, 31, 10, 10, 10),
+				openTime: 900,
+				joinTeleconfBeforeHost: true,
+				duration: 20,
+				timezoneID: 4
+			})
+			.createMeeting();
+
+		t.is(resp, '<Success />');
+	} catch (e) {
+		console.log(e);
+	}
+
+
+
 });
 
 	//
